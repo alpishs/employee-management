085ca12a2c703d0f441835775e6646220982a64e
alpishs <alpishs@yahoo.com>
2026-06-30 14:49:48 +0530

    update base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://employee-management-api.onrender.com",
+  baseURL: "https://employee-management-api-8y57.onrender.com",
 });
 
 export default api;
